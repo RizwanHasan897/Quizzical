cf5433c36975fa4b4cf241856348da3eb0a6d121
rizwan hasan <rizwanhasan897@gmail.com>
2022-10-31 22:26:18 +0000

    applicaion

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react"
+import StartScreen from "./Componants/StartScreen"
+import QuizScreen from "./Componants//QuizScreen"
+import Loader from "./Componants//Loader"
+import './app.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+    const [startGame, setStartGame] = useState(false)
+    const [quizData, setQuizData] = useState([])
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    function getQuizData() {
+        setQuizData([])
+        fetch("https://opentdb.com/api.php?amount=5&category=9&type=multiple")
+            .then(res => res.json())
+            .then(data => {
+                
+                data.results.map( obj => {
+                    const allAns = obj.incorrect_answers
+                    const num = Math.floor(Math.random() * 4)
+                    allAns.splice(num, 0, obj.correct_answer)
+                    obj.all_answers = allAns
+                    return obj
+                })
+                setQuizData(data.results)
+            })
+    }
+    
+    
+    function handleStart() {
+        setStartGame(true)
+        getQuizData()
+    }
+
+    
+    return (
+        <div className="body-content">
+            {!startGame 
+                ? <StartScreen handleStart={handleStart} /> 
+                : quizData.length === 0 
+                ? <Loader />
+                : <QuizScreen getQuizData={getQuizData} quizData={quizData} />
+            }
+        </div>
+    )
 }
-
-export default App
